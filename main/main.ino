ccee9a0c09857ab8869887fd9907387def57e299
kearllan aeymard del castillo <delcastilllokearllan@gmail.com>
2021-05-09 00:05:59 +0800

    Initial Commit

---
+++ main/main.ino
@@ -0,0 +1,17 @@
+/*
+ * Automatic Sampayan (PUP 2021)
+ * 
+ * Rain Sensor
+ * DC MOTOR
+ * UV Lights
+ * FAN
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
